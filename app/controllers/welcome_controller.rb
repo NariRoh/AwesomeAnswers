b36ccd9b2b7d01df4761c82a12464c00e63d48c8
Nari Roh <nari.rohh@gmail.com>
2017-02-07 20:04:22 -0800

    Intoro to Rails: day22

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,32 @@
+class WelcomeController < ApplicationController
+  # this is the Index action
+  def index
+    # render({plain: 'Hello World!'})
+    # render plain: 'Hello World!'
+
+    # this will automatically look for a file names: index.html.erb
+    # within folder: /app/views/welcome
+    # it defaults to `.html` because the default `format` for requests in
+    # rubyonrails is always HTML unless you specify otherwise (like json..
+    # http://localhost:3000/index.html or .json)
+    # erb: is the default built-in templating system with Rails
+    # By default Rails will use `app/views/layouts/application.html.erb` as
+    # the layout, so the content of `home.html.erb` will be inserted in the
+    # place of `<%= yield %>` within the layout
+    # render :home
+
+    # the default behaviour is to render: index.html.erb within folder:
+    # /app/views/welcome for each action  🖕 action's name.html.erb
+  end
+
+  def contact_us
+  end
+
+  def contact_submit
+    # params gives us access to all GET or POST parameters from the HTTP request
+    # params uses Hashes with indifferent access which means it will work with
+    # either symbols or strings as keys
+    @name = params[:name]
+    # render plain: 'Form submitted'
+  end
+end
